6ebea585e3162b78467b3bb79e39ab1b041409c5
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-11-03 05:51:39 -0300

    Add routes to routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,10 +24,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
