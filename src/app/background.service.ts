423effb2cd61ee5abf8bbc1aa8ab66d7d430edf6
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-11-03 05:49:25 -0300

    Add a service to manage background add and remove attribute.

---
+++ src/app/background.service.ts
@@ -0,0 +1,51 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Juliano Maciel Ferreira.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+import { Injectable, Renderer2, RendererFactory2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackgroundService {
+
+  private renderer: Renderer2;
+  private body: HTMLElement;
+
+  constructor(
+    private rendererFactory: RendererFactory2,
+    @Inject(DOCUMENT) private document: any
+  ) {
+
+    this.renderer = rendererFactory.createRenderer(null, null);
+    this.body = this.document.getElementsByTagName('body')[0];
+  }
+
+  public setBackgroundImage(): void {
+    this.renderer.setAttribute(this.body, 'id', 'bg-img');
+  }
+
+  public removeBackgroundImage(): void {
+    this.renderer.removeAttribute(this.body, 'id');
+  }
+}
